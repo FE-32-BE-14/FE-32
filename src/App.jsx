44d37e3ab3863db106bcad3765398f229feb1c9a
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2023-06-15 16:38:19 +0700

    add todolist page and route

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import "./App.css"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import PageDonasi from "./pages/PageDonasi"
-import PagePembayaran from "./pages/PagePembayaran"
-import PageAcceptPembayaran from "./pages/PageAcceptPembayaran"
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PageDonasi from "./pages/PageDonasi";
+import PagePembayaran from "./pages/PagePembayaran";
+import PageAcceptPembayaran from "./pages/PageAcceptPembayaran";
+import PageTodoList from "./pages/PageTodoList";
 
 function App() {
   return (
     <>
-    <Router>
-      <Routes>
-        <Route path="/" element={<PageDonasi/>}/>
-        <Route path="/pembayaran" element={<PagePembayaran/>}/>
-        <Route path="/accept" element={<PageAcceptPembayaran/>}/>
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<PageDonasi />} />
+          <Route path="/pembayaran" element={<PagePembayaran />} />
+          <Route path="/accept" element={<PageAcceptPembayaran />} />
+          <Route path="/todo-list" element={<PageTodoList />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
