e9b799a604d889fd599ebe46665bedb6c8899364
ronald0312 <rosalinaaldastar@gmail.com>
2023-06-16 21:34:32 +0700

    add verifikasi

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import "./App.css"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import PageDonasi from "./pages/PageDonasi"
-import PagePembayaran from "./pages/PagePembayaran"
-import PageAcceptPembayaran from "./pages/PageAcceptPembayaran"
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PageDonasi from "./pages/PageDonasi";
+import PagePembayaran from "./pages/PagePembayaran";
+import PageAcceptPembayaran from "./pages/PageAcceptPembayaran";
+import CustomNavbar from "./components/Navbar/Navbar";
+import { VerifikasiProvider } from "../context/VerifikasiContext.";
+import VerifikasiList from "./components/Verifikasi/VerifikasiList.jsx";
 
 function App() {
   return (
-    <>
     <Router>
-      <Routes>
-        <Route path="/" element={<PageDonasi/>}/>
-        <Route path="/pembayaran" element={<PagePembayaran/>}/>
-        <Route path="/accept" element={<PageAcceptPembayaran/>}/>
-      </Routes>
+      <VerifikasiProvider>
+        <CustomNavbar/>
+        <Routes>
+          <Route path="/" element={<VerifikasiList />} />
+          <Route path="/donasi" element={<PageDonasi />} />
+          <Route path="/pembayaran" element={<PagePembayaran />} />
+          <Route path="/accept" element={<PageAcceptPembayaran />} />
+        </Routes>
+      </VerifikasiProvider>
     </Router>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
