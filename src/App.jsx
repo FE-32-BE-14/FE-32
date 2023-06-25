74cb077847b83577988c4af68d6e910f5d91b530
ronald0312 <rosalinaaldastar@gmail.com>
2023-06-25 12:33:16 +0700

    upload login,register,verifikasi terbaru

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider, AuthContext } from './context/AuthContext';
+import { VerifikasiProvider } from './context/VerifikasiContext';
+import LoginPage from './components/Login-Verifikasi/Login';
+import RegisterPage from './components/Login-Verifikasi/Register';
+import VerifikasiSuccess from './components/Login-Verifikasi/VerifikasiSuccess';
 import PageDonasi from "./pages/PageDonasi";
 import PagePembayaran from "./pages/PagePembayaran";
-import PageAcceptPembayaran from "./pages/PageAcceptPembayaran";
+import AcceptPembayaran from './components/AcceptPembayaran/AcceptPembayaran';
+import VerifikasiPage from './pages/VerifikasiPage';
 
-function App() {
+const App = () => {
   return (
-    <>
-      <Routes>
-        {/* masukan endpoint donasi bila layar white screen */}
-        <Route path="/donasi" element={<PageDonasi />} />
-        <Route path="/pembayaran" element={<PagePembayaran />} />
-        <Route path="/accept" element={<PageAcceptPembayaran />} />
-      </Routes>
-    </>
+    <AuthProvider>
+      <VerifikasiProvider>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <AuthContext.Consumer>
+                {({ isLoggedIn }) =>
+                  isLoggedIn ? <PageDonasi /> : <Navigate to="/login" replace={true} />
+                }
+              </AuthContext.Consumer>
+            }
+          />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/verifikasi" element={<VerifikasiPage />} />
+          <Route path="/verifikasi-success" element={<VerifikasiSuccess />} />
+          <Route path="/pembayaran" element={<PagePembayaran />} />
+          <Route path="/accept" element={<AcceptPembayaran />} />
+          <Route path="/donasi" element={<PageDonasi />} />
+        </Routes>
+      </VerifikasiProvider>
+    </AuthProvider>
   );
-}
+};
 
 export default App;
